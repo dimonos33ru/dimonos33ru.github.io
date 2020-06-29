77df9c368695c5e55d900c5ce013f9067ec5101b
dimonos33ru <36456015+dimonos33ru@users.noreply.github.com>
2020-06-29 14:41:17 +0300

    first upload

---
+++ CncAssistent/precache-manifest.ab5b8bc644805621d5539e0eb4c38598.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "366252200ad341c2873d51549c467026",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25957e0c6826186351f9",
+    "url": "/static/css/2.42ec2a3e.chunk.css"
+  },
+  {
+    "revision": "3a6ec522b3ded16ab3d0",
+    "url": "/static/css/main.4e8f442e.chunk.css"
+  },
+  {
+    "revision": "25957e0c6826186351f9",
+    "url": "/static/js/2.131a2671.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.131a2671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a6ec522b3ded16ab3d0",
+    "url": "/static/js/main.2977eda0.chunk.js"
+  },
+  {
+    "revision": "e661affb54845a79021d",
+    "url": "/static/js/runtime-main.d16d2295.js"
+  }
+]);
